2d20a1ee5f67267bd8530bed270e2bdd2092c5b1
Markus Suomalainen <markus.suomalainen@gmail.com>
2019-03-31 14:06:56 +0300

    traned model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import keras
 from net import mnist_conv
 from keras.datasets import mnist
 
+# Get the mnist dataset and normalize the data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
@@ -10,3 +11,19 @@ x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
+
+# Convert to binary class matrix
+y_train = keras.utils.to_categorical(y_train, 10, dtype='float32')
+y_test = keras.utils.to_categorical(y_test, 10, dtype='float32')
+
+# Get and compile
+model = mnist_conv(input_shape)
+model.load_weights('model.h5')
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#model.fit(x_train, y_train, epochs=4, batch_size=512)
+
+# Save model
+#model.save_weights('model.h5')
+
+test = model.evaluate(x_test, y_test)
+print(test)
--- net.py
+++ net.py
@@ -4,14 +4,13 @@ from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
 
 
 def mnist_conv(input_size):
-
     X_input = Input(input_size)
-    X = Conv2D(32, kernel_size=(3,3))(X_input)
-    X = MaxPool2D(pool_size=(2,2))(X)
+    X = Conv2D(32, kernel_size=(3, 3))(X_input)
+    X = MaxPool2D(pool_size=(2, 2))(X)
     X = Flatten()(X)
-    X = Dense(128, activation='leaky_relu')(X)
+    X = Dense(128, activation='relu')(X)
     X = Dropout(0.2)(X)
-    X = Dense(10, activation='softmax')
+    X = Dense(10, activation='softmax')(X)
 
     model = models.Model(inputs=X_input, outputs=X)
 
